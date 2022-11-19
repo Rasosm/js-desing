72cc1c9f2c9261eecf81e0caeb37a29d8e200d72
Rasosm <rasosm@gmail.com>
2022-11-19 18:40:33 +0200

    Hobbies js

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,42 +1,53 @@
 // IMPORTS
-
+import renderHobbies from "../components/hobbies.js";
 // EXECUTION
+(async () => {
+    /* HEADER-start */
+    /* HEADER-end */
 
-/* HEADER-start */
-/* HEADER-end */
+    /* HERO-start */
+    /* HERO-end */
 
-/* HERO-start */
-/* HERO-end */
+    /* ABOUT-ME-start */
+    /* ABOUT-ME-end */
 
-/* ABOUT-ME-start */
-/* ABOUT-ME-end */
+    /* HOBBIES-start */
+    try {
+        const response = await fetch('./data/hobbies.json');
+        const data = await response.json();
+        const hobbiesResponse = renderHobbies('hobbies', data);
+        if (hobbiesResponse[0]) {
+            console.error(hobbiesResponse[1]);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+    /* HOBBIES-end */
 
-/* HOBBIES-start */
-/* HOBBIES-end */
+    /* ACHIEVEMENTS-start */
+    /* ACHIEVEMENTS-end */
 
-/* ACHIEVEMENTS-start */
-/* ACHIEVEMENTS-end */
+    /* SERVICES-start */
+    /* SERVICES-end */
 
-/* SERVICES-start */
-/* SERVICES-end */
+    /* WORK-start */
+    /* WORK-end */
 
-/* WORK-start */
-/* WORK-end */
+    /* EXPERTISE-start */
+    /* EXPERTISE-end */
 
-/* EXPERTISE-start */
-/* EXPERTISE-end */
+    /* PORTFOLIO-start */
+    /* PORTFOLIO-end */
 
-/* PORTFOLIO-start */
-/* PORTFOLIO-end */
+    /* TESTIMONIALS-start */
+    /* TESTIMONIALS-end */
 
-/* TESTIMONIALS-start */
-/* TESTIMONIALS-end */
+    /* HIRE-ME-start */
+    /* HIRE-ME-end */
 
-/* HIRE-ME-start */
-/* HIRE-ME-end */
+    /* CONTACTS-start */
+    /* CONTACTS-end */
 
-/* CONTACTS-start */
-/* CONTACTS-end */
-
-/* FOOTER-start */
-/* FOOTER-end */
+    /* FOOTER-start */
+    /* FOOTER-end */
+})();
